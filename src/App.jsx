297f73df2f6c024f93c255990a4f02d59a7c29e7
Ashutosh Singh <19ashutoshsingh@gmail.com>
2024-08-20 02:22:43 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import './App.css'
 import Chat from './components/chat/Chat'
 import Detail from './components/detail/Detail'
-import List from './components/list/list'
+import List from './components/list/List'
 import Login from './components/login/Login';
 import Notification from './components/notification/Notification';
 import { onAuthStateChanged } from 'firebase/auth';
